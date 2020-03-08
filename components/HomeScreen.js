a698f616192b386c94766ddb2ca513556e960d40
skeola <46700392+skeola@users.noreply.github.com>
2020-03-07 19:28:33 -0800

    Fixed button shapes to be quarter circles. Need to find a way to make it scale dynamically with screen size.

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -7,30 +7,34 @@ import { block } from 'react-native-reanimated';
 const HomeScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-          <TouchableOpacity 
-            style={styles.button}
-            onPress={() => navigation.navigate("Screen1")}
-          >
-            <Text style={styles.buttonText}>Screen 1</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.button}
-            onPress={() => navigation.navigate("Screen2")}
-          >
-            <Text style={styles.buttonText}>Screen 2</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.button}
-            onPress={() => navigation.navigate("Screen3")}
-          >
-            <Text style={styles.buttonText}>Screen 3</Text>
-          </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.button}
-            onPress={() => navigation.navigate("Screen4")}
-          >
-            <Text style={styles.buttonText}>Screen 4</Text>
-          </TouchableOpacity>
+          <View style={styles.top}>
+            <TouchableOpacity 
+              style={[styles.button, styles.TL]}
+              onPress={() => navigation.navigate("Screen1")}
+            >
+              <Text style={styles.buttonText}>Screen 1</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={[styles.button, styles.TR]}
+              onPress={() => navigation.navigate("Screen2")}
+            >
+              <Text style={styles.buttonText}>Screen 2</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bottom}>
+            <TouchableOpacity 
+              style={[styles.button, styles.BL]}
+              onPress={() => navigation.navigate("Screen3")}
+            >
+              <Text style={styles.buttonText}>Screen 3</Text>
+            </TouchableOpacity>
+            <TouchableOpacity 
+              style={[styles.button, styles.BR]}
+              onPress={() => navigation.navigate("Screen4")}
+            >
+              <Text style={styles.buttonText}>Screen 4</Text>
+            </TouchableOpacity>
+          </View>
         </View>
     );
 };
@@ -41,20 +45,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    alignContent: 'center',
   },
-  // button: {
-  //   borderRadius: '50%',
-  //   width: 40,
-  //   height: 40,
-  //   transform: [{ rotate: '45deg' }],
-  //   borderWidth: 20,
-  //   borderStyle: 'solid',
-  //   borderColor: 'red green blue yellow',
-  //   margin: 10,
-  // },
-  // buttonText: {
-  //   color: '#fff',
-  // },
+  top: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  bottom: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    height: 200,
+    width: 200,
+    borderStyle: 'solid',
+    borderColor: 'blue',
+    borderWidth: 10,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TL: {
+    borderTopLeftRadius: 200,
+  },
+  TR: {
+    borderTopRightRadius: 200,
+  },
+  BL: {
+    borderBottomLeftRadius: 200,
+  },
+  BR: {
+    borderBottomRightRadius: 200,
+  },
+  buttonText: {
+    color: 'black',
+  },
 });
 
 export default HomeScreen;
