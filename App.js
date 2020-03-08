cd89ec97185147938b811678c582b810961a00c3
Reed Hummel <reed.hummel@gmail.com>
2020-03-08 14:56:10 -0700

    Add icon for back button
    We got the back button for free from the react stack navigation library,
    but this was customizing the back button to have the icon. This was an
    initial POC, as it currently only works on Screen 1 and would need to be
    copy-pasted to count for other pages.
    
    Next step is to add this to a class that our screens are derived from.

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from './components/HomeScreen';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./components/HomeScreen";
+import backIcon from "./assets/backIcon.png";
 
 const Screen1 = () => {
   return (
-      <View style={styles.container}>
-        <Text>I am Screen 1</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>I am Screen 1</Text>
+    </View>
   );
 };
 
 const Screen2 = () => {
   return (
-      <View style={styles.container}>
-        <Text>I am Screen 2</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>I am Screen 2</Text>
+    </View>
   );
 };
 
 const Screen3 = () => {
   return (
-      <View style={styles.container}>
-        <Text>I am Screen 3</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>I am Screen 3</Text>
+    </View>
   );
 };
 
 const Screen4 = () => {
   return (
-      <View style={styles.container}>
-        <Text>I am Screen 4</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>I am Screen 4</Text>
+    </View>
   );
 };
 
@@ -41,25 +41,34 @@ const Stack = createStackNavigator();
 
 const App = () => {
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Screen1" component={Screen1} />
-          <Stack.Screen name="Screen2" component={Screen2} />
-          <Stack.Screen name="Screen3" component={Screen3} />
-          <Stack.Screen name="Screen4" component={Screen4} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Screen1"
+          component={Screen1}
+          options={({ navigation, route }) => ({
+            headerBackImage: props => (
+              <img src={backIcon} width="10%" length="10%" /
+            )
+          })}
+        />
+        <Stack.Screen name="Screen2" component={Screen2} />
+        <Stack.Screen name="Screen3" component={Screen3} />
+        <Stack.Screen name="Screen4" component={Screen4} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
+//header: ({ goBack }) => ({left: ( <Icon name={'chevron-left'} onPress={ () => { goBack() } }  /> )})
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
