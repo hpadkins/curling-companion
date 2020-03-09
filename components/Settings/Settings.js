7590750a0573f1076232bc6a546ca993b510ab60
hpadkins <hadkins@pdx.edu>
2020-03-09 13:37:12 -0700

    Settings button with overlay

--- components/Settings/Settings.js
+++ components/Settings/Settings.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { Overlay } from 'react-native-elements';
 import { MaterialIcons } from 'react-native-vector-icons';
 
 export default class Settings extends Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            isVisible: false
+        };
     }
+
+    openingO = () => {
+        this.setState({isVisible:!this.state.isVisible})
+    }
+
     render(){
         return(
             <View style={styles.container}>
-             <Text>Settings!!!</Text>
-                 <MaterialIcons name='settings' />
-            </View>
-        )
+             
+             <MaterialIcons 
+                    name='settings'
+                    size={25}
+    
+                    onPress={this.openingO} />
+             {this.state.isVisible?
+        <Overlay
+            isVisible={this.state.isVisible}
+            width="auto"
+            height="auto" 
+            onBackdropPress={this.openingO}
+            >
+        <Button  title="Vibrate" onPress={()=>console.log("press")} />
+        <Button  title="Sound" onPress={()=>console.log("press")} />
+        <Button  title="Hi" onPress={()=>console.log("press")} />
+        </Overlay>:null}
+                 
+        </View>
+        );
     }
 }
   
