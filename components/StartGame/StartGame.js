991d9dac5e429dae4bc30e93a6724d23311d031c
TuNguy3n <tu4@pdx.edu>
2020-03-30 12:41:17 -0700

    Update StartGame.js
    add display current end & player function

--- components/StartGame/StartGame.js
+++ components/StartGame/StartGame.js
@@ -4,15 +4,37 @@ import { View, Text, Image} from 'react-native';
 
 class StartGame extends Component 
 {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Image source={require('./img/curling.svg')} 
-                       style={{width: 400, height: 400}} />
-                <Text> Number of Ends </Text>
-            </View>
-        );
+  constructor() {
+    super();
+    this.state = { 
+      currentEnd: 0,
+      currentPlayer: 0,
+    };
+  }
+
+  displayCurrentPlayer(curentPlayer) {
+    if(curentPlayer < 2) {
+      return <Text>PLAYER 1</Text>
+    } else if (curentPlayer < 4) {
+      return <Text>PLAYER 2</Text>
+    } else if (curentPlayer < 6){
+      return <Text>PLAYER 3</Text>
+    } else {
+      return <Text>PLAYER 4</Text>
     }
+  }
+
+  render() {
+    //Pass #ends from PreGameScreen 
+    const numbOfEnds = this.props.route.params.numbOfEnds; 
+
+    return (
+      <View style={styles.container}>
+        <Text>END: {this.state.currentEnd +1}</Text>
+        {this.displayCurrentPlayer(this.state.currentPlayer)}
+      </View>
+    );
+  }
 };
 
 export default StartGame;
